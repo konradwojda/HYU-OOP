332ed7511b72daa21aacba523a8d297b5bbd3cfe
Konrad Wojda <konrad.wojda@comp.com.pl>
2025-05-05 18:41:42 +0900

    add hw05

---
+++ 05-hw/Main.java
@@ -0,0 +1,70 @@
+class _Character {
+    protected char ch;
+    private static int objectCounter = 0;
+
+    public _Character() {
+        objectCounter++;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public static int getObjectCounter() {
+        return objectCounter;
+    }
+}
+
+class _Digit extends _Character {
+    public _Digit(char ch) {
+        super();
+        this.ch = ch;
+    }
+
+    public int plus(_Digit other) {
+        int a = Character.getNumericValue(this.ch);
+        int b = Character.getNumericValue(other.ch);
+        return (a + b) % 10;
+    }
+}
+
+class _Letter extends _Character {
+    public _Letter(char ch) {
+        super();
+        this.ch = ch;
+    }
+}
+
+class _Vowel extends _Letter {
+    public _Vowel(char ch) {
+        super(ch);
+    }
+}
+
+class _Consonant extends _Letter {
+    public _Consonant(char ch) {
+        super(ch);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        _Digit d1 = new _Digit('5');
+        _Digit d2 = new _Digit('6');
+        _Letter l1 = new _Letter('a');
+        _Letter l2 = new _Letter('b');
+        _Vowel v1 = new _Vowel('e');
+        _Vowel v2 = new _Vowel('i');
+        _Consonant c1 = new _Consonant('k');
+        _Consonant c2 = new _Consonant('m');
+        _Character ch1 = new _Character();
+        _Character ch2 = new _Character();
+
+        System.out.println("Result of d1.plus(d2): " + d1.plus(d2));  // Expected: 1
+        System.out.println("Total object count: " + _Character.getObjectCounter());  // Expected: 10
+    }
+}
